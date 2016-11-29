8910d0ac4cf78a91c5efbc57fa6cfb0a3782e72b
Derek <derek@dadian-smith.net>
2016-11-28 23:55:00 -0600

    Add locations array

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var locations = [
+  {
+    name: 'Smoking Barrels',
+    location: {
+      lat: -90.28124,
+      lng: 38.579166
+    }
+  },
+  {
+    name: 'Our Lady of Sorrows',
+    location: {
+      lat: -90.28336,
+      lng: 38.577946
+    }
+  }
+];
