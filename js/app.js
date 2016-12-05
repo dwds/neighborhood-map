37616cd4ca35403bb7ae5d5baa015bdd6e650b27
Derek <derek@dadian-smith.net>
2016-12-04 19:06:46 -0600

    Fix coordinates
    Latitude/longitude were reversed

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@ var locations = [
   {
     name: 'Smoking Barrels',
     location: {
-      lat: -90.28124,
-      lng: 38.579166
+      lat: 38.579166,
+      lng: -90.28124
     }
   },
   {
     name: 'Our Lady of Sorrows',
     location: {
-      lat: -90.28336,
-      lng: 38.577946
+      lat: 38.577946,
+      lng: -90.28336
     }
   }
 ];
