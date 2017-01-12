3185c74adc11715c83aa81a27da263c3bb469d8c
Derek <derek@dadian-smith.net>
2017-01-11 19:40:06 -0600

    Add comments

--- js/app.js
+++ js/app.js
@@ -100,23 +100,31 @@ $.each(locations, function(key, location) {
   });
 });
 
+
+// Google map
 var map;
 function initMap() {
+  // initialize map
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 38.579166, lng: -90.28124},
     zoom: 15
   });
+  // setup map info for each location
   $.each(locations, function(key, location) {
+    // create marker, and place it on map by default
     location.marker = new google.maps.Marker({
       position: location.position,
+      // setting map value to null will remove marker from map
       map: map,
       title: location.name,
       label: location.name[0]
     });
+    // create infoWindow
     location.infoWindow = new google.maps.InfoWindow({
       content: location.name
     });
     // TODO: separate this out into a named function
+    // when marker is clicked, animate it and open its infoWindow
     location.marker.addListener('click', function() {
       location.infoWindow.open(map, location.marker);
       if (location.marker.getAnimation() == null) {
@@ -134,34 +142,49 @@ function initMap() {
 var ViewModel = function () {
   var self = this;
 
+  // initialize locationList as empty array
   self.locationList = ko.observableArray([]);
 
+  // set filter to 'all' by default
   self.filter = ko.observable('all');
 
+  // create initial locationList (all locations)
   locations.forEach(function(location){
     self.locationList.push(location);
   });
 
+  // change the locationList based on the selected filter value
   self.filterLocations = function() {
+    // clear locationList
     self.locationList([]);
+
+    // if filter value is 'all', add all locations to locationList
+    // and add all markers to the map
     if(self.filter() === 'all') {
       locations.forEach(function(location){
         self.locationList.push(location);
         location.marker.setMap(map);
       });
     } else {
+      // for each location ...
       $.each(locations, function(key, location) {
+        // if the selected filter value is its list of location types
         if($.inArray(self.filter(), location.type) > -1) {
+          // add location to location list
           self.locationList.push(location);
+          // add marker to map
           location.marker.setMap(map);
         } else {
+          // remove marker from map
           location.marker.setMap(null);
         }
       });
     }
   };
 
+  // when the filter value is changed, call the filterLocations method
   self.filter.subscribe(self.filterLocations);
 }
 
+// hook it all up
 ko.applyBindings(new ViewModel());
